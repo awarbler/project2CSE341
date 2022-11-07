7cd6b257bd7fb4893e6213aeab86b5cc9506d632
[awarbler] <[awoodfor@byui.edu]>
2022-11-06 18:04:55 -0600

    tt

--- routes/app.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// router.get('/', function (req, res, next) {
-//   res.redirect('/api-docs');
-// });
-// module.exports = router;
